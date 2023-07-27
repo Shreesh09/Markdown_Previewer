3c901d34d23c10cbed885aa55edbd93577caf8b2
Shreesh <shreeshnautiyal3@gmail.com>
2023-07-27 19:18:48 +0530

    refactoring

--- src/redux/text_to_JSX.js
+++ src/redux/text_to_JSX.js
@@ -1,13 +1,10 @@
 import { marked } from 'marked';
 
 const convertToJSX = (text) => {
-    text = text.split("").map((c) => {
-         if(c.charCodeAt(0) !== 10 && c.charCodeAt(0) !== 13)
-             return c;
-         else return "  \n";
-     }).join("");
-
     text = text.replace(/(\\r)|(\\n)/g, "  \n");
+    marked.use({
+        breaks: true,
+    });
     const JSX = marked.parse(text);
     return (JSX);
     };
